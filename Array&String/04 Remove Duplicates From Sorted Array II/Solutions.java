94113482f4e1833d7b7c6265b31acc2f0b2aa67c
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-07-25 17:53:44 +0700

    finishing problem 04

--- Array&String/04 Remove Duplicates From Sorted Array II/Solutions.java
+++ Array&String/04 Remove Duplicates From Sorted Array II/Solutions.java
@@ -1,9 +1,34 @@
 public class Solutions {
     public static void main(String[] args) {
+
+        int[] nums = new int[]{1,1,1,2,2,3};
+        int[] nums = new int[]{1,1,2,2,3,3};
         
     }
 
     public int removeDuplicates(int[] nums) {
+
+        if (nums.length < 3) {
+
+            return nums.length;
+
+        } else {
+
+            int pointer = 2;
+
+            for (int i = 2; i < nums.length; i++) {
+                if (nums[i] > nums[pointer-2]) {
+                    nums[pointer++] = nums[i];
+                } 
+            }
+
+            return pointer;
+        }
         
+
     }
 }
+
+// dicek sama index kedua sebelum index dia
+// kalo sama update increase
+// kalo beda biarin

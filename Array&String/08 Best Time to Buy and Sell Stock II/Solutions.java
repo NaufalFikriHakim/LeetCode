b6f47360aadc4b51819087b2a2cb1973ae6b6625
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-07-29 11:38:44 +0700

    finished problem 08

---
+++ Array&String/08 Best Time to Buy and Sell Stock II/Solutions.java
@@ -0,0 +1,17 @@
+public class Solutions {
+    public static void main(String[] args) {
+        
+    }
+
+    public int maxProfit(int[] prices){
+        int profit = 0;
+        
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i+1] > prices[i]) {
+                profit += prices[i+1] - prices[i];
+            }
+        }
+
+        return profit;
+    }
+}
